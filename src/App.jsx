3dfbc8e7a90d295c3b1b58f0c8a529971544127c
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2024-09-12 15:14:38 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import OverviewPage from "./pages/OverviewPage";
+import Siderbar from "./components/Siderbar";
+
+function App() {
+ return (
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+        {/* Background */}
+        <div className="fixed inset-0 z-0">
+            <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
+            <div className="absolute inset-0 backdrop-blur-sm" />
+        </div>
+
+        <Siderbar />
+        <Routes>
+            <Route path="/" element={<OverviewPage />} />
+            <Route path="/products" element={<ProductsPage/>} />
+        </Routes>
+    </div>
+ )
+};
+
+export default App;
