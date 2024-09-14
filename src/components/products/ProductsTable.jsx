bd9710957d0bf0067e768f3b78ee67a296a5e801
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2024-09-15 00:05:17 +0100

    product name

--- src/components/products/ProductsTable.jsx
+++ src/components/products/ProductsTable.jsx
@@ -11,6 +11,17 @@ const PRODUCT_DATA = [
 ];
 
 const ProductsTable = () => {
+ const [ searchTerm, setSearchTerm] = useState("");
+ const [ filteredProducts, setFilteredProducts] = useState(PRODUCT_DATA);
+
+ const handleSearch = (e) => {
+  const term = e.target.value.toLowerCase();
+  setSearchTerm(term);
+  const filtered = PRODUCT_DATA.filter(product => product.name.toLowerCase().includes(term)  || product.category.toLowerCase().includes(term));
+
+  setFilteredProducts(filtered);
+ }
+
   return (
     <motion.div
         className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border-gray-700 mb-8"
@@ -25,9 +36,67 @@ const ProductsTable = () => {
             type="text" 
             placeholder="Search Products..."
             className="bg-gray-700 text-white placeholder-gray-400 rounded-lg pl-10 pr-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            onChange={handleSearch}
+            value={searchTerm}
           />
           <Search className="absolute left-3 top-2.5 text-gray-400" size={18} />
        </div>
+      </div>
+
+      <div className="overflow-x-auto">
+         
+         <table className="min-w-full divide-y divide-gray-700">
+         <thead>
+            <tr>
+              <th
+                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
+              >
+                Name
+              </th>
+              <th
+                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
+              >
+                Category
+              </th>
+              <th
+                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
+              >
+                Price
+              </th>
+              <th
+                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
+              >
+                Stock
+              </th>
+              <th
+                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
+              >
+                Sales
+              </th>
+              <th
+                className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
+              >
+                Actions
+              </th>
+            </tr>
+          </thead>
+            <tbody className="divide-y divide-gray-700"></tbody>
+                 {filteredProducts.map(product =>(
+                  <motion.tr key={product.id}
+                   initial={{ opacity: 0 }}
+                   animate={{ opacity: 1 }}
+                   transition={{ duration: 0.3 }}
+                  >
+                    <td className="px-6 py-4 whitespace-nowrap tet-sm font-medium text-gray-100 flex gap-2 items-center">
+                      <img src="https://images.unsplash.com/photo-1627989580309-bfaf3e58af6f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d2lyZWxlc3MlMjBlYXJidWRzfGVufDB8fDB8fHww" alt="product img"  
+                          className="size-10 rounded-full" 
+                      />
+                      {product.name}
+                    </td>
+                    
+                  </motion.tr>
+                 ))}
+         </table>
       </div>
     </motion.div>
   )
