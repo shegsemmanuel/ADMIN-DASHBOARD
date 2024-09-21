6c7f172dbfe392dbabe5f2bb43058bf70b2639e9
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2024-09-21 01:07:58 +0100

    analytic insight

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -5,6 +5,7 @@ import ChannelPerformance from "../components/analytics/ChannelPerformance";
 import ProductPerformance from "../components/analytics/ProductPerformance";
 import UserRetention from "../components/analytics/UserRetention";
 import CustomerSegmentation from "../components/analytics/CustomerSegmentation";
+import AIPoweredInsights from "../components/analytics/AIPoweredInsights";
 
 const AnalyticsPage = () => {
   return (
@@ -21,6 +22,7 @@ const AnalyticsPage = () => {
                <UserRetention />
                <CustomerSegmentation />
             </div>
+            <AIPoweredInsights />
         </main>
     </div>
   );
